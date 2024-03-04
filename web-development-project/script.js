fb9e9eaa6a71c92f2dead012bfcbacb2cd967252
meyou2me <ka8627378@gmail.com>
2024-03-04 15:10:30 +0100

    Add initial web development project files

---
+++ web-development-project/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Function to display a greeting message
+function displayGreeting() {
+    alert('Welcome to my web development project!');
+}
+
+// Call the displayGreeting function when the page is loaded
+window.onload = function() {
+    displayGreeting();
+}
